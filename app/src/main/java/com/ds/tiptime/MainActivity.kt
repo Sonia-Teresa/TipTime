cdd2f97f2701b3bfedf2d4706a6f2db3fbcff820
charlenefialho <fialhocharlene@gmail.com>
2022-04-13 22:55:36 -0300

    calculargorjeta

--- app/src/main/java/com/ds/tiptime/MainActivity.kt
+++ app/src/main/java/com/ds/tiptime/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ds.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ds.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
         setContentView(R.layout.activity_main)
     }
 }
